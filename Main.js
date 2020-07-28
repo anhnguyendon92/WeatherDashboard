e0cfdd12ec85ae1933a477a07ce4e4a7d6bf35d7
Anh Nguyen <anhnguyendon92@gmail.com>
2020-07-27 19:46:13 -0700

    Done

--- Main.js
+++ Main.js
@@ -1,21 +1,83 @@
-let app = "0f3e11117cf4815f182186649c2179fa";
-let units = "imperial";
-let searchMethod;
+const form = document.querySelector(".top-banner form");
+const input = document.querySelector(".top-banner input");
+const msg = document.querySelector(".top-banner .msg");
+const list = document.querySelector(".ajax-section .cities");
 
-function searchWeather(searchTerm) {
-    fetch('http://api.openweathermap.org/data/2.5/weather?$("searchMethod")=$(searchTerm)&APPID=$(appID)&units=$(units)').then(results => {
-        return results.json();
-    }).then(results => {
-        init(result);
+const apiKey = "0f3e11117cf4815f182186649c2179fa";
+
+form.addEventListener("submit", e => {
+  e.preventDefault();
+  let inputVal = input.value;
+
+  //check if there's already a city
+  const listItems = list.querySelectorAll(".ajax-section .city");
+  const listItemsArray = Array.from(listItems);
+
+  if (listItemsArray.length > 0) {
+    const filteredArray = listItemsArray.filter(el => {
+      let content = "";
+      //athens,gr
+      if (inputVal.includes(",")) {
+        //athens,grrrrrr->invalid country code, so we keep only the first part of inputVal
+        if (inputVal.split(",")[1].length > 2) {
+          inputVal = inputVal.split(",")[0];
+          content = el
+            .querySelector(".city-name span")
+            .textContent.toLowerCase();
+        } else {
+          content = el.querySelector(".city-name").dataset.name.toLowerCase();
+        }
+      } else {
+        //athens
+        content = el.querySelector(".city-name span").textContent.toLowerCase();
+      }
+      return content == inputVal.toLowerCase();
+    });
+
+    if (filteredArray.length > 0) {
+      msg.textContent = `You already know the weather for ${
+        filteredArray[0].querySelector(".city-name span").textContent
+      } ...otherwise be more specific by providing the country code as well 😉`;
+      form.reset();
+      input.focus();
+      return;
+    }
+  }
+
+  //ajax here
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${inputVal}&appid=${apiKey}&units=metric`;
+
+  fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      const { main, name, sys, weather } = data;
+      const icon = `https://s3-us-west-2.amazonaws.com/s.cdpn.io/162656/${
+        weather[0]["icon"]
+      }.svg`;
+
+      const li = document.createElement("li");
+      li.classList.add("city");
+      const markup = `
+        <h2 class="city-name" data-name="${name},${sys.country}">
+          <span>${name}</span>
+          <sup>${sys.country}</sup>
+        </h2>
+        <div class="city-temp">${Math.round(main.temp)}<sup>°C</sup></div>
+        <figure>
+          <img class="city-icon" src="${icon}" alt="${
+        weather[0]["description"]
+      }">
+          <figcaption>${weather[0]["description"]}</figcaption>
+        </figure>
+      `;
+      li.innerHTML = markup;
+      list.appendChild(li);
     })
-}
+    .catch(() => {
+      msg.textContent = "Please search for a valid city 😩";
+    });
 
-function init(resultFromServer) {
-    console.log(resultFromServer);
-}
-
-document.getElementById('searchBtn').addEventListener('click', () => {
-    let searchTerm = document.getElementById('searchInput').value;
-    if(searchTerm)
-        searchWeather(searchTerm);
-})
+  msg.textContent = "";
+  form.reset();
+  input.focus();
+});
--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # WeatherDashboard
+
+Search up Cities to see what the weather is!
+
+When you type in the city of your choosing the forcast at the time will show up. Sunny, cloudy, rain, snow etc
+
+Check it out!
